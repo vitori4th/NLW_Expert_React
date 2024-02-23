444f14aca9e4dfde0f5286e93a458e9a2c838ed6
Vitória Thaís <vitoriathdasilva@gmail.com>
2024-02-23 11:15:28 -0300

    add Toaster

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,18 +1,26 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import { X } from 'lucide-react'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import {toast} from 'sonner'
 
 export function NewNoteCard() {
     const [shouldShowOnBoarding, setShouldShowOnBoarding] = useState(true)
+    const [content, setContent] = useState('')
 
     function handleStartEditor() {
         setShouldShowOnBoarding(false)
     }
 
     function handleContentChange(event: ChangeEvent<HTMLTextAreaElement>) {
+        setContent(event.target.value)
         if (event.target.value === '') {
             setShouldShowOnBoarding(true)
         }
+    }
+
+    function handleSaveNote(event: FormEvent) {
+        event.preventDefault()
+        toast.success('Nota criada com sucesso')
     }
 
     return (
@@ -32,29 +40,31 @@ export function NewNoteCard() {
                     <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
                         <X className='size-5' />
                     </Dialog.Close>
-                    <div className='flex flex-1 flex-col gap-3 p-5 '>
-                        <span className='text-sm font-medium text-slate-300'>
-                            Adicionar nota
-                        </span>
-                        {shouldShowOnBoarding ? (
-                            <p className='text-sm leading-6 text-slate-400'>
-                                Comece <button className='font-md text-lime-400 hover:underline'>gravando uma nota</button> em áudio ou se preferir <button onClick={handleStartEditor} className='font-md text-lime-400 hover:underline'> utilize apenas texto</button>.
-                            </p>
-                        ) : (
-                            <textarea
-                                autoFocus
-                                className='text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none'
-                                onChange={handleContentChange}
-                            ></textarea>
-                        )}
-                    </div>
+                    <form onSubmit={handleSaveNote} className='flex-1 flex flex-col'>
+                        <div className='flex flex-1 flex-col gap-3 p-5 '>
+                            <span className='text-sm font-medium text-slate-300'>
+                                Adicionar nota
+                            </span>
+                            {shouldShowOnBoarding ? (
+                                <p className='text-sm leading-6 text-slate-400'>
+                                    Comece <button className='font-md text-lime-400 hover:underline'>gravando uma nota</button> em áudio ou se preferir <button onClick={handleStartEditor} className='font-md text-lime-400 hover:underline'> utilize apenas texto</button>.
+                                </p>
+                            ) : (
+                                <textarea
+                                    autoFocus
+                                    className='text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none'
+                                    onChange={handleContentChange}
+                                ></textarea>
+                            )}
+                        </div>
 
-                    <button
-                        type='button'
-                        className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500'
-                    >
-                        Salvar nota
-                    </button>
+                        <button
+                            type='submit'
+                            className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500'
+                        >
+                            Salvar nota
+                        </button>
+                    </form>
                 </Dialog.Content>
             </Dialog.Portal>
         </Dialog.Root>
