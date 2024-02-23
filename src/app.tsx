74fcf627b25630e1c1436b6ad9b2ec5bee3aca5f
Vitória Thaís <vitoriathdasilva@gmail.com>
2024-02-23 01:07:20 -0300

    create modal

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,11 @@ import logo from './assets/nlw-expert-logo.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+const note ={
+  date: new Date(),
+  content: 'Heyyy'
+}
+
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -19,9 +24,7 @@ export function App() {
 
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note}/>
       </div>
     </div>
   )
